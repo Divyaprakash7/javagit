e171f6a2818d38b854bc545624fc8300e07a2779
Divyaprakash7 <117444033+Divyaprakash7@users.noreply.github.com>
2023-01-10 23:38:20 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 class Hello{
 public static void main(String args[])
 {
+int a=5,b=6;
+System.out.println("sum "+(a+b));
+System.out.println("diff "+(a-b));
+System.out.println("mul "+(a*b));
+System.out.println("div "+(a/b));
 System.out.println("Hello World!");
 }
 }
+
